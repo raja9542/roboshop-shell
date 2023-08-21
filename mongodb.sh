c1b663bc875223216096b6e94eeb565d70ca8544
raja9542 <rajasekhar1banda@gmail.com>
2023-08-21 11:03:35 +0530

    frontend

---
+++ mongodb.sh
@@ -0,0 +1,12 @@
+script_location=$(pwd)
+
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+systemctl start momgod
+
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
+systemctl restart mongod
