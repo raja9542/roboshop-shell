ef87c5a608f7f5a01473d82484ef6c75e1e61ab1
raja9542 <rajasekhar1banda@gmail.com>
2023-08-24 12:34:53 +0530

    frontend

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ status_check() {
   if [ $? -eq 0 ]; then
     echo -e "\e[1;32m SUCCESS \e[0m"
     else
-      echo -e "\e[1;32m FAILURE \e[0m"
+      echo -e "\e[1;31m FAILURE \e[0m"
       echo -e "Refer Log file for more information,Log- ${LOG}"
   exit
       fi
