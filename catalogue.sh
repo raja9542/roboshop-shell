e95256768f026f9da6c9503002ae625d563df62e
raja9542 <rajasekhar1banda@gmail.com>
2023-08-24 12:53:00 +0530

    frontend

--- catalogue.sh
+++ catalogue.sh
@@ -1,47 +1,47 @@
 source common.sh
-echo -e "\e[1;35m Configuring NodeJS Repos\e[0m"
+print_head "Configuring NodeJS Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
-echo -e "\e[1;35m Installing NOdeJS\e[0m"
+print_head "Installing NOdeJS"
 yum install nodejs -y &>>${LOG}
 status_check
-echo -e "\e[1;35m Add Application USER\e[0m"
+print_head "Add Application USER"
 useradd roboshop &>>${LOG}
 status_check
 mkdir -p /app &>>${LOG}
 status_check
-echo -e "\e[1;35m Download APP Content\e[0m"
+print_head  "Download APP Content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 status_check
-echo -e "\e[1;35m Deleting APP Old Content\e[0m"
+print_head  "Deleting APP Old Content"
 rm -rf /app/* &>>${LOG}
 status_check
-echo -e "\e[1;35m Extracting APP Content\e[0m"
+print_head "Extracting APP Content"
 cd /app
 unzip /tmp/catalogue.zip &>>${LOG}
 status_check
-echo -e "\e[1;35m Installing NodeJS Dependencies\e[0m"
+print_head "Installing NodeJS Dependencies"
 cd /app
 npm install &>>${LOG}
 status_check
-echo -e "\e[1;35m Configuring Catalogue Service file\e[0m"
+print_head  "Configuring Catalogue Service file"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
 status_check
-echo -e "\e[1;35m Reload Systemd\e[0m"
+print_head  "Reload Systemd"
 systemctl daemon-reload &>>${LOG}
 status_check
-echo -e "\e[1;35m Enable Catalogue Service\e[0m"
+print_head  "Enable Catalogue Service"
 systemctl enable catalogue &>>${LOG}
 status_check
-echo -e "\e[1;35m Start Catalogue Service\e[0m"
+print_head  "Start Catalogue Service"
 systemctl start catalogue &>>${LOG}
 status_check
-echo -e "\e[1;35m Configure Mongo Repo\e[0m"
+print_head "Configure Mongo Repo"
 cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
-echo -e "\e[1;35m Install Mongo Client\e[0m"
+print_head "Install Mongo Client"
 yum install mongodb-org-shell -y &>>${LOG}
 status_check
-echo -e "\e[1;35m Load Schema\e[0m"
+print_head "Load Schema"
 mongo --host mongodb-dev.devopsraja66.online </app/schema/catalogue.js &>>${LOG}
 status_check
--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,23 @@
 source common.sh
-echo -e "\e[36m Install nginx\e[0m"
+print_head "Install nginx"
 yum install nginx -y &>>${LOG}
 status_check
-echo -e "\e[36m Remove Nginx old content\e[0m"
+print_head  "Remove Nginx old content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
 status_check
-echo -e "\e[36m Download Frontend Content\e[0m"
+print_head "Download Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 cd /usr/share/nginx/html/ &>>${LOG}
 status_check
-echo -e "\e[36m Extract Frontend Content\e[0m"
+print_head "Extract Frontend Content"
 unzip /tmp/frontend.zip &>>${LOG}
 status_check
-echo -e "\e[36m Copy Roboshop Nginx Config file\e[0m"
+print_head "Copy Roboshop Nginx Config file"
 cp ${script_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 status_check
-echo -e "\e[36m enable nginx\e[0m"
+print_head "enable nginx"
 systemctl enable nginx &>>${LOG}
 status_check
-echo -e "\e[36m start nginx\e[0m"
+print_head "start nginx"
 systemctl restart nginx &>>${LOG}
 status_check
