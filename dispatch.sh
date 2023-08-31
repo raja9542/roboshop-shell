b9621cc40e888e02a79d3f260b5adc3220fb30ca
raja9542 <rajasekhar1banda@gmail.com>
2023-08-31 18:32:40 +0530

    frontend

--- dispatch.sh
+++ dispatch.sh
@@ -1,10 +1,4 @@
 source common.sh
-
-if [ -z "${roboshop_rabbitmq_password}" ]; then
-  echo -e "Variable roboshop_rabbitmq_password is Needed"
-  exit
-fi
-
 component=dispatch
 schema_load=false
 
