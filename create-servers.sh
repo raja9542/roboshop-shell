41eaf51260da7a8e0689a0f879d66cc835e56ac1
raja9542 <rajasekhar1banda@gmail.com>
2023-11-04 12:22:43 +0530

    nexus frontend artifact push

--- create-servers.sh
+++ create-servers.sh
@@ -12,8 +12,7 @@ create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
-      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
@@ -46,6 +45,66 @@ for component in catalogue cart user shipping payment frontend mongodb mysql rab
   COMPONENT="${component}-${env}"
   create_ec2
 done
+
+
+
+
+
+
+
+
+
+
+
+
+##!/bin/bash
+#
+###### Change these values ###
+#ZONE_ID="Z09171912J6RDBH9U9MN3"
+#DOMAIN="devopsraja66.online"
+#SG_NAME="Devops"
+#env=dev
+##############################
+#
+#
+#create_ec2() {
+#  PRIVATE_IP=$(aws ec2 run-instances \
+#      --image-id ${AMI_ID} \
+#      --instance-type t3.micro \
+#      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+#      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+#      --security-group-ids ${SGID} \
+#      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+#
+#  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
+#  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
+#  if [ $? -eq 0 ]; then
+#    echo "Server Created - SUCCESS - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+#  else
+#     echo "Server Created - FAILED - DNS RECORD - ${COMPONENT}.${DOMAIN}"
+#     exit 1
+#  fi
+#}
+#
+#
+### Main Program
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+#if [ -z "${AMI_ID}" ]; then
+#  echo "AMI_ID not found"
+#  exit 1
+#fi
+#
+#SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+#if [ -z "${SGID}" ]; then
+#  echo "Given Security Group does not exit"
+#  exit 1
+#fi
+#
+#
+#for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
+#  COMPONENT="${component}-${env}"
+#  create_ec2
+#done
 
 
 
